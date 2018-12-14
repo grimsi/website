9f04582ca014ba24b32e1f9e847a751a171f2308
Simon Grimme <simon@grimme-online.de>
2018-12-14 20:13:18 +0100

    Update: CRT Filter applied, Lorem Ipsum Text inserted TODO: CRT Scroll, Scrollbars

---
+++ app/main.js
@@ -0,0 +1,98 @@
+"use strict";
+window.onload = function () { setTestText(); };
+function setTestText() {
+    var terminal = document.getElementById("terminal");
+    if (terminal) {
+        terminal.innerText = "Welcome to grimsi.de 1.0.0 LTS (OS 4.15.0-39-generic x86_64)\n" +
+            "System information as of Thu Dec 13 19:48:23 CET 2018\n" +
+            "\n" +
+            "System load:  0.84                Processes:              181\n" +
+            "Usage of /:   25.0% of 153.60GB   Users logged in:        2\n" +
+            "Memory usage: 32%                 IP address for ens3:    188.68.54.205\n" +
+            "Swap usage:   0%                  IP address for docker0: 172.17.0.1\n" +
+            "\n" +
+            "* Security for grimsi.de!\n" +
+            "We will soon support SSL!\n" +
+            "\n" +
+            " * This site is in the process of being reworked.\n" +
+            "   - please be patient\n" +
+            "\n" +
+            "0 Software-Pakete können aktualisiert werden.\n" +
+            "0 Aktualisierungen sind Sicherheitsaktualisierungen.\n" +
+            "\n" +
+            "\n" +
+            "*** Neustart des Systems erforderlich ***\n" +
+            "Last login: Thu Dec 13 19:33:50 2018 from 87.183.54.156\n" +
+            "grimsi@v22017125440758296:~$\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n" +
+            "test\n";
+    }
+}
